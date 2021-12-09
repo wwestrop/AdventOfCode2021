b0b0d2b8e60d9a4aeef774bcb89be11d3fcb1b11
Will Westrop <wwestrop@hotmail.com>
2021-12-09 19:29:37 +0000

    Day 9, doesn't work on the full input

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -4,6 +4,13 @@
     {
         public static void Main()
         {
+            Day09.Run();
+            // I'm not even going to attempt Day 8.
+            // I'm doing this after spending the day at work,
+            // and then doing all my other chores.
+            // The description reads like a <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            // I mean, COME. ON. Have SOME respect for my time.
+            // It'll probably take an hour to unpick the deliberate obtuseness
             Day07.Run();
             Day06.Run();
             Day05.Run();
